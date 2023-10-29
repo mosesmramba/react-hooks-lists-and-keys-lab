0a55b5d5dc7a54bd1bf51409c8461dc8fac97358
mosesmramba <moses.mramba@student.moringaschool.com>
2023-10-29 15:28:38 +0300

    Completed Assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,14 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{
+    /* display an <a> tag for each link here */
+    <div>
+      <a href="#home">home</a>
+      <a href="#about">about</a>
+      <a href="#projects">projects</a>
+    </div>
+    }</nav>;
 }
 
 export default NavBar;
